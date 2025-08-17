70c668642cd496dbf9961f7bf8e22a0e52adb186
Nafish32 <imti2305101930@diu.edu.bd>
2025-08-17 21:19:11 +0600

    fixed compile error

--- src/main/java/com/example/homes/HomesApplication.java
+++ src/main/java/com/example/homes/HomesApplication.java
@@ -5,9 +5,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HomesApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HomesApplication.class, args);
-	}
-
+	public static void main(String[] args) { SpringApplication.run(HomesApplication.class, args); }
 }
